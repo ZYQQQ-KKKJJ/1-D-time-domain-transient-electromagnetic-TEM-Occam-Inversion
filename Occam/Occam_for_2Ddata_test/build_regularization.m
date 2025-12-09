bc45fb39ee84dfdc8ba41cafdbfe7bd586e03bc9
ZYQ <yqzhao24@mails.jlu.edu.cn>
2025-12-09 14:30:11 +0800

    Update build_regularization.m

--- Occam/Occam_for_2Ddata_test/build_regularization.m
+++ Occam/Occam_for_2Ddata_test/build_regularization.m
@@ -11,24 +11,3 @@ function R = build_regularization(nlayer)
     end
 end
 
-% function [R, Re, Rt] = build_regularization(nrho, nthk, w_rho, w_thk)
-%     % 电阻率正则化
-%     Re = zeros(nrho-2, nrho);
-%     for i = 1:nrho-2
-%         Re(i, i) = 1;
-%         Re(i, i+1) = -2;
-%         Re(i, i+2) = 1;
-%     end
-%     Re = w_rho * Re;
-% 
-%     % 厚度正则化
-%     Rt = zeros(nthk-2, nthk);
-%     for i = 1:nthk-2
-%         Rt(i, i) = 1;
-%         Rt(i, i+1) = -2;
-%         Rt(i, i+2) = 1;
-%     end
-%     Rt = w_thk * Rt;
-% 
-%     R = blkdiag(Re, Rt);  % 合并成总正则
-% end
